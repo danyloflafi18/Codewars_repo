394773f6cd000b25f86ec4d1f3b5f6ad1296b68f
danyloflafi18 <d.chalyi16@gmail.com>
2020-12-16 15:04:42 +0200

    JWT 16.12

--- Gemfile
+++ Gemfile
@@ -1,11 +1,13 @@
 source 'https://rubygems.org'
 
+
+gem 'atlassian-jwt'
 gem 'capybara'
 gem 'chromedriver-helper'
 gem 'cucumber'
 gem 'pry'
 gem 'rake'
-gem 'rest-client'
 gem 'require_all'
+gem 'rest-client'
 gem 'rspec'
 gem 'selenium-webdriver'
--- jwt_sample.rb
+++ jwt_sample.rb
@@ -1,15 +1,17 @@
 require 'atlassian/jwt'
+require 'active_support/time'
+require 'date'
 
 require 'uri'
 require 'net/http'
 
 # The URL of the API call, must include the query string, if any
-url = 'https://jira.atlassian.com/rest/api/latest/issue/JRA-9'
-base_url = 'https://chalyi20.atlassian.net'
+url = 'https://prod-api.zephyr4jiracloud.com/connect/public/rest/api/1.0/cycle'
+base_url = 'https://prod-api.zephyr4jiracloud.com/connect/'
 # The key of the app as defined in the app description
 issuer = 'MjA1NDAyYjAtMWQ2YS0zYzk4LWE1NzktY2Q2ODEwNGM1OWNiIDVjZjY4NGQ3ZDlmNmI5MGYzZDRiMTRkNCBVU0VSX0RFRkFVTFRfTkFNRQ'
 # The HTTP Method (GET, POST, etc) of the API call
-http_method = 'get'
+http_method = 'post'
 # The shared secret returned when the app is installed
 shared_secret = 'UzrlAZxg1F5KEe-MYiNvPfAUW4Cpqo-L-lQW1gyGvMg'
 
@@ -18,13 +20,27 @@ claim = Atlassian::Jwt.build_claims(
   url,
   http_method,
   base_url,
+  (Time.now - 60.seconds).to_i,
+  (Time.now + 1.day).to_i
+=begin
+  claim={:iat => Time.at(Time.now - 60.seconds),
+         :exp => Time.at(Time.now + 1.day)}
+=end
 )
-jwt = JWT.encode(claim, shared_secret)
 
-uri = URI("https://prod-api.zephyr4jiracloud.com/connect/public/rest/api/latest/issue/JRA-9?jwt=#{jwt}")
+jwt = JWT.encode(claim, shared_secret)
+require 'pry'
+binding.pry
+puts jwt
+
+uri = URI(url)
 http = Net::HTTP.new(uri.host, uri.port)
-http.set_debug_output($stdout)
-request = Net::HTTP::Get.new(uri.request_uri)
+request = Net::HTTP::Post.new(uri.request_uri)
+headers = {
+  content_type: 'application/json',
+  authorization: jwt,
+  zapiaccesskey: issuer
+}
+request.initialize_http_header(headers)
 response = http.request(request)
 
-claims, jwt_header = Atlassian::Jwt.decode(params[:jwt], nil, false)
